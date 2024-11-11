ec5649d7969fc845d16f521a2fe7a317b7cf67ce
PooriaEshghi <pooriaeshghi@gmail.com>
2024-11-11 17:15:22 +0100

    Adding CRUD method to employeeDAO

--- src/main/java/com/pooriaeshghi/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/pooriaeshghi/cruddemo/dao/EmployeeDAO.java
@@ -6,4 +6,10 @@ import com.pooriaeshghi.cruddemo.entity.Employee;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
